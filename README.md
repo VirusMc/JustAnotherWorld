dffbddd9c4fefd4e537d988b3fc37815ae9950bc
VirusMc <virus.mc.tlstudios@gmail.com>
2023-04-16 07:23:17 +0200

    INIT

---
+++ README.md
@@ -0,0 +1,7 @@
+# Just Another World
+
+This Project was created by pure boredom.
+
+The purpose is to take all day Objects and represent it in code.
+
+The Main is basically just to test the current code.
---
+++ src/main/java/de/virusmc/projects/justanotherworld/JustAnotherWorldMain.java
@@ -0,0 +1,8 @@
+package de.virusmc.projects.justanotherworld;
+
+public class JustAnotherWorldMain {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
